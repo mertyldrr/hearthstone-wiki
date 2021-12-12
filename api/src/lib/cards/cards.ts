45ef976a1e58d3e1f156bd0086449795aea47a6e
mertyldrr <mertyildirir14@gmail.com>
2021-12-12 18:09:56 +0100

    axios installed, dynamodb helper functions added

--- api/src/lib/cards/cards.ts
+++ api/src/lib/cards/cards.ts
@@ -2,26 +2,34 @@ import { dynamoClient } from "../../config/dynamo";
 
 const TABLE_NAME = "HS_CARDS";
 
-const getCards = async () => {
+export const getCards = async () => {
   const params = {
     TableName: TABLE_NAME,
   };
   try {
     const characters = await dynamoClient.scan(params).promise();
-    console.log(characters);
     return characters;
   } catch (error) {
     console.log(error);
   }
 };
 
-const cardx = {
-  cardId: "10",
-  name: "abc",
-  type: "water",
+export const getCardById = async (id: string) => {
+  const params = {
+    TableName: TABLE_NAME,
+    Key: {
+      id,
+    },
+  };
+  try {
+    const character = await dynamoClient.get(params).promise();
+    return character;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-const addCard = async (card: object) => {
+export const addorUpdateCard = async (card: object) => {
   try {
     const params = {
       TableName: TABLE_NAME,
@@ -31,12 +39,4 @@ const addCard = async (card: object) => {
   } catch (error) {
     console.log(error);
   }
-};
-
-addCard(cardx);
-// getCards();
-
-module.exports = {
-  getCards,
-  addCard,
 };
