70d93077ae899e6dd4a0bf5aa020dfc8c413fff5
mertyldrr <mertyildirir14@gmail.com>
2022-01-09 22:31:36 +0100

    redux dev tools installed, redux-logger removed

--- src/redux/store/index.ts
+++ src/redux/store/index.ts
@@ -1,13 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from '@redux-saga/core';
-import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import rootReducer from './rootReducer';
 import { rootSaga } from './rootSaga';
 
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(rootReducer, applyMiddleware(sagaMiddleware, logger));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
+);
 
 sagaMiddleware.run(rootSaga);
 
