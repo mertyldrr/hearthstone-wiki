620d9489c862af9c00e7325cedcf7553c3f53e22
mertyldrr <mertyildirir14@gmail.com>
2022-03-22 23:03:16 +0100

    enable same search functionality for different components

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,20 +1,40 @@
 import { GlobalState } from '../redux/store/rootReducer';
 import { SearchBarStyled } from './styles/Header.styled';
-import { searchCardsByName } from '../redux/actions/cards';
+import { searchCardsByName } from '../redux/actions/cards/cards';
+import { searchByCardbackName } from '../redux/actions/cardbacks/cardbacks';
 import { connect, useDispatch } from 'react-redux';
+import { useLocation } from 'react-router-dom';
 
 const SearchBar = (props: any) => {
+  const location = useLocation();
   const dispatch = useDispatch();
-  const onChangeHandler = (event: string) => {
-    dispatch(
-      searchCardsByName({
-        searchText: event,
-      })
-    );
-  };
+  let onChangeHandler;
+  if (location.pathname === '/cards') {
+    onChangeHandler = (event: string) => {
+      dispatch(
+        searchCardsByName({
+          searchText: event,
+        })
+      );
+    };
+  } else if (location.pathname === '/cardbacks') {
+    onChangeHandler = (event: string) => {
+      dispatch(
+        searchByCardbackName({
+          searchText: event,
+        })
+      );
+    };
+  }
+
   const onCancelHandler = () => {
     dispatch(
       searchCardsByName({
+        searchText: '',
+      })
+    );
+    dispatch(
+      searchByCardbackName({
         searchText: '',
       })
     );
